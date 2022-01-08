ab611fd0c509f42d2e42bf0b1d4b6f279b465fe6
MujurID <mujurdotid@gmail.com>
2022-01-08 21:54:47 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,16 +4,17 @@ const hdWallet = require('ethereumjs-wallet/hdkey') //npm i ethereumjs-wallet@0.
 var vanityAddressFoundA = false;
 
 //const regA = /0xDEPAN.*BELAKANG$/;
-const regA = /0xdead.*$/;
+const regA = /0xdead0.*$/;
 
 var mnemonic;
 var seed;
 var HdKey;
 var node;
 var address;
+var privatekey;
 
 while(!vanityAddressFoundA){
-
+	process.stdout.write(".");
     mnemonic = bip39.generateMnemonic();
 	//console.log(`\nMnemonic: ${mnemonic}`);
     seed = bip39.mnemonicToSeedSync(mnemonic);
@@ -21,8 +22,9 @@ while(!vanityAddressFoundA){
     HdKey = hdWallet.fromMasterSeed(seed);
     node = HdKey.derivePath("m/44'/60'/0'/0/0");
     address = node.getWallet().getChecksumAddressString();
+	//privatekey = node.privateExtendedKey();
     
-    console.log(`\nAddress: ${address}`);
+    //console.log(`\nAddress: ${address}`);
     if(regA.test(address) && !vanityAddressFoundA){
         vanityAddressFoundA = true;
         console.log(`\nAddress: ${address}\nMnemonic: ${mnemonic}`);
