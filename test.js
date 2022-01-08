1379cfdbc3d32e3f9483763d0e93ba6e6318b894
MujurID <mujurdotid@gmail.com>
2022-01-08 15:58:42 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+const bip39 = require('bip39');
+const hdWallet = require('ethereumjs-wallet/hdkey')
+
+var vanityAddressFoundA = false;
+
+//const regA = /0xAA.*11$/;
+const regA = /0xAA.*$/;
+
+var mnemonic;
+var seed;
+var HdKey;
+var node;
+var address;
+
+while(!vanityAddressFoundA){
+
+    mnemonic = bip39.generateMnemonic();
+    seed = bip39.mnemonicToSeed(mnemonic);
+    HdKey = hdWallet.fromMasterSeed(seed);
+    node = HdKey.derivePath("m/44'/60'/0'/0/0");
+    address = node.getWallet().getChecksumAddressString();
+    
+    console.log(`\nAddress: ${address});
+    if(regA.test(address) && !vanityAddressFoundA){
+        vanityAddressFoundA = true;
+        console.log(`\nAddress: ${address}\nMnemonic: ${mnemonic}`);
+        }
+    }
+}
