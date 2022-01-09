b69d28accd470403583f4c1d2c97c6bc7878c3ea
MujurID <mujurdotid@gmail.com>
2022-01-09 14:20:45 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const bip39 = require('bip39'); //npm i bip39@3.0.4
 const hdWallet = require('ethereumjs-wallet/hdkey') //npm i ethereumjs-wallet@0.6.5
-	
+
 var vanityAddressFoundA = false;
 
 //const regA = /0xDEPAN.*BELAKANG$/;
@@ -9,7 +9,7 @@ var vanityAddressFoundA = false;
 const add_awal = 'dead';
 const add_akhir = '';
 
-const regA0 = '0x'+`${add_awal}`+'.*'+`${add_akhir}`+'$';
+const regA0 = '0x' + `${add_awal}` + '.*' + `${add_akhir}` + '$';
 
 const regA = new RegExp(regA0);
 
@@ -22,20 +22,20 @@ var node;
 var address;
 var privatekey;
 
-while(!vanityAddressFoundA){
-	process.stdout.write(".");
+while (!vanityAddressFoundA) {
+    process.stdout.write(".");
     mnemonic = bip39.generateMnemonic();
-	//console.log(`\nMnemonic: ${mnemonic}`);
+    //console.log(`\nMnemonic: ${mnemonic}`);
     seed = bip39.mnemonicToSeedSync(mnemonic);
-	//console.log(`\nSeed: ${seed}`);
+    //console.log(`\nSeed: ${seed}`);
     HdKey = hdWallet.fromMasterSeed(seed);
     node = HdKey.derivePath("m/44'/60'/0'/0/0");
     address = node.getWallet().getChecksumAddressString();
-	//privatekey = node.privateExtendedKey();
-    
+    //privatekey = node.privateExtendedKey();
+
     //console.log(`\nAddress: ${address}`);
     //if(regA.test(address.toLowerCase()) && !vanityAddressFoundA){
-	if(regA.test(address.toLowerCase()) && !vanityAddressFoundA){
+    if (regA.test(address) && !vanityAddressFoundA) {
         vanityAddressFoundA = true;
         console.log(`\nAddress: ${address}\nMnemonic: ${mnemonic}`);
     }
